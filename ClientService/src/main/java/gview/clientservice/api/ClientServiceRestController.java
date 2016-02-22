9b1f10d0f8b84f8e119bf8eb523c88df34b745fd
xbarrufet <xbarrufetm@gmail.com>
2016-02-22 21:18:28 +0100

    1

--- ClientService/src/main/java/gview/clientservice/api/ClientServiceRestController.java
+++ ClientService/src/main/java/gview/clientservice/api/ClientServiceRestController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClientServiceRestController {
 
-	@RequestMapping("/client")
+	/*@RequestMapping("/client")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
-    }
+    }*/
 }
