b4e1883077b6d6878ae81070a714f6c01b7a36f6
xbarrufet <xbarrufetm@gmail.com>
2016-02-25 08:05:34 +0100

    test amb assured

--- ClientService/src/main/java/gview/clientservice/api/ClientContractDTO.java
+++ ClientService/src/main/java/gview/clientservice/api/ClientContractDTO.java
@@ -11,6 +11,11 @@ public class ClientContractDTO {
 	private String location;
 	private int dayOfWeek;
 	private List<String> tasks;
+	
+	public ClientContractDTO() {
+		
+	}
+	
 	public String getEmail() {
 		return email;
 	}
@@ -31,6 +36,34 @@ public class ClientContractDTO {
 	}
 	public List<String> getTasks() {
 		return tasks;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setGardenId(String gardenId) {
+		this.gardenId = gardenId;
+	}
+
+	public void setContractId(String contractId) {
+		this.contractId = contractId;
+	}
+
+	public void setGardenName(String gardenName) {
+		this.gardenName = gardenName;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public void setDayOfWeek(int dayOfWeek) {
+		this.dayOfWeek = dayOfWeek;
+	}
+
+	public void setTasks(List<String> tasks) {
+		this.tasks = tasks;
 	}
 	
 	
--- ClientService/src/test/java/test/gview/clientservice/TestClientService.java
+++ ClientService/src/test/java/test/gview/clientservice/TestClientService.java
@@ -1,12 +1,62 @@
 package test.gview.clientservice;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Response;
 
-import gview.clientservice.api.ClientServiceRestController;
+import gview.clientservice.ClientServiceAppConfig;
+import gview.clientservice.api.ClientContractDTO;
 
+import static com.jayway.restassured.RestAssured.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ClientServiceAppConfig.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
 public class TestClientService {
-
- }
+		@Value("${local.server.port}")   // 6
+	    int port;
+		
+		 @Before
+		 public void setUp() {
+			 RestAssured.port = port;
+		 }	 
+		 
+		 @Test
+		 public void canAddClientContract() {
+			 
+			 ClientContractDTO dto = new ClientContractDTO();
+			 dto.setContractId("contractid");
+			 dto.setEmail("xbarrufetm@gmail.com");
+			 dto.setGardenId("gardenId");
+			 dto.setLocation("location");
+			 dto.setDayOfWeek(5);
+			 //Response nomes estar per printar, pot començar directament amb give()
+			 Response response = given().
+			 	contentType("application/json").
+		        body(dto).
+			 when().
+			    post("/v1/clients");
+			 response.then().statusCode(200);
+			 System.out.println(response.asString());
+			 /* sense consola i amb expect -->
+			 given().
+			 	contentType("application/json").
+		        body(dto).
+			 when().
+			    post("/v1/clients");
+			 then().
+				statusCode(200);
+				*/
+	     }
+}
+	  
+		 
